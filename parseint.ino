617c97ce531862c2751f37c28559c71bb474edbe
Prakat <prakatsharma064@gmail.com>
2026-06-13 10:35:41 +0545

    use of serial.parseint and serial.available

---
+++ parseint.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+
+    int value = Serial.parseInt();
+
+    Serial.print("Received: ");
+    Serial.println(value);
+  }
+}
